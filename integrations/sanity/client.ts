b6bef438659e57648dedb12f0d1393ad378e55e2
luxrugcare-cmyk <luxrugcare@gmail.com>
2026-08-13 20:24:38 +0200

    fix: pin verified Sanity project id in client

--- integrations/sanity/client.ts
+++ integrations/sanity/client.ts
@@ -2,10 +2,12 @@ import { createClient } from "next-sanity";
 import { sanityEnv, isSanityConfigured } from "./env";
 
 const readToken = process.env.SANITY_API_READ_TOKEN;
+const projectId = "g5y9wcb1";
 
 export const sanityClient = isSanityConfigured
   ? createClient({
       ...sanityEnv,
+      projectId,
       useCdn: !readToken,
       perspective: "published",
       token: readToken || undefined,
