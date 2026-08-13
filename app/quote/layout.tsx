6a836020f02306d3e1b8a2a44e4f79c921b7e714
luxrugcare-cmyk <luxrugcare@gmail.com>
2026-08-14 01:20:10 +0200

    analytics: track residential funnel start

---
+++ app/quote/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import { FunnelStartTracker } from "@/components/analytics/FunnelStartTracker";
+
+export default function QuoteLayout({ children }: { children: ReactNode }) {
+  return <><FunnelStartTracker event="quote_start" leadType="residential" />{children}</>;
+}
